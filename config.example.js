c7c2f51afb7ae7ad0300b1d635f0bca9c4b1c311
donbkim <74445521+donbkim@users.noreply.github.com>
2026-09-20 16:40:31 +0900

    chore: add config.example.js

---
+++ config.example.js
@@ -0,0 +1,4 @@
+window.DASHBOARD_CONFIG = {
+  // Apps Script /exec URL. Copy this file to config.js before publishing.
+  endpoint: 'https://script.google.com/macros/s/AKfycbxh000yHsJNkro5qKkc6vpT-JrhtMJraSROGYN3eZ6lxIGpsEAx_rJ6KpVgP82h0pytcA/exec'
+};
